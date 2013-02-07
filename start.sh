ad8df705e100bf6739827becd52ddb3e8a228291
Jeremy <jeremykentc@gmail.com>
2013-02-07 07:34:57 -0500

    reworking the config so it will scale better

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s resource_manager -config sys.config -name resource_manager -setcookie cookie
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s resource_manager -config sys.config -name resource_manager@virtualhold.com -setcookie cookie
