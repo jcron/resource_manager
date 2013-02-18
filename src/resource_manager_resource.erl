4b1a66a398a4604192e664e31d7071271a8e3c4e
Jeremy <jeremykentc@gmail.com>
2013-02-18 08:07:24 -0500

    renaming internal function

--- src/resource_manager_resource.erl
+++ src/resource_manager_resource.erl
@@ -31,16 +31,17 @@ content_types_provided(ReqData, State) ->
     {[{"application/json", to_json}], ReqData, State}.
 
 from_json(ReqData, State) ->
-    Action = wrq:path_info(action, ReqData),
     try
+        Action = wrq:path_info(action, ReqData),
         Segment = get_segment(ReqData),
+        Conversation = get_conversation(ReqData),
         case Action of
-            "checkout" -> checkout_resource(ReqData, State, Segment);
-            "checkin"  -> checkin_resource(ReqData, State, Segment);
-            _          -> bad_request(ReqData, State)
+            "checkout" -> checkout_resource(ReqData, State, Segment, Conversation);
+            "checkin"   -> checkin_resource(ReqData, State, Segment, Conversation);
+            _              -> bad_request(ReqData, State)
         end
     catch
-        no_resource -> json_response(ReqData, State, rm_json:error(no_resource));
+        no_resource -> no_resource(ReqData, State);
         _Error:_Reason -> {false, ReqData, State}
     end.
     
@@ -51,20 +52,34 @@ to_json(ReqData, State) ->
 all_resources(ReqData, State) ->
     Segments = rm_librarian:get_all_segments(),
     SegmentStruct = get_segments_json(Segments, []),
-    to_json(ReqData, State, rm_json:segments(SegmentStruct)).
+    encode_to_json(ReqData, State, rm_json:segments(SegmentStruct)).
     
 bad_request(ReqData, State) ->
     json_response(ReqData, State, rm_json:error(bad_request)).
 
-checkin_resource(ReqData, State, Segment) ->
-    {Total, Available} = rm_librarian:check_in_resource(Segment),
+checkin_resource(ReqData, State, Segment, Conversation) ->
+    {Total, Available} = rm_librarian:check_in_resource(Segment, Conversation),
     SegmentStruct = rm_json:segment_detail(Segment, Total, Available),
     json_response(ReqData, State, rm_json:segments(SegmentStruct)).
 
-checkout_resource(ReqData, State, Segment) ->
-    {Total, Available} = rm_librarian:check_out_resource(Segment),
+checkout_resource(ReqData, State, Segment, Conversation) ->
+    {Total, Available} = rm_librarian:check_out_resource(Segment, Conversation),
     SegmentStruct = rm_json:segment_detail(Segment, Total, Available),
     json_response(ReqData, State, rm_json:segments(SegmentStruct)).    
+
+encode_to_json(ReqData, State, Json) ->
+    {mochijson2:encode(Json), ReqData, State}.
+    
+get_content_value(ReqData, Content) ->
+    Body = wrq:req_body(ReqData),
+    {struct, Json} = mochijson2:decode(Body),
+    case proplists:get_value(Content, Json) of
+        undefined -> [];
+        Value -> binary_to_list(Value)
+    end.
+
+get_conversation(ReqData) ->
+    get_content_value(ReqData, <<"id">>).
 
 get_segment(ReqData) ->
     get_segment(ReqData, wrq:method(ReqData)).
@@ -72,9 +87,7 @@ get_segment(ReqData) ->
 get_segment(ReqData, 'GET') ->
     wrq:get_qs_value("segment", ReqData);
 get_segment(ReqData, 'PUT') ->
-    Body = wrq:req_body(ReqData),
-    {struct, Json} = mochijson2:decode(Body),
-    binary_to_list(proplists:get_value(<<"segment">>, Json)).
+    get_content_value(ReqData, <<"segment">>).
 
 get_segments_json([], JsonStruct) ->
     JsonStruct;
@@ -88,17 +101,17 @@ json_response(ReqData, State, Response) ->
     ReturnJson = iolist_to_binary(ReturnIo),
     R2 = wrq:append_to_resp_body(ReturnJson, ReqData),
     {true, R2, State}.
-    
+
+no_resource(ReqData, State) ->
+    json_response(ReqData, State, rm_json:error(no_resource)).
+
 show_resources(ReqData, State) ->
     case get_segment(ReqData) of
         undefined -> all_resources(ReqData, State);
         Segment ->
             SegmentStruct = get_segments_json([Segment], []),
-            to_json(ReqData, State, rm_json:segments(SegmentStruct))
+            encode_to_json(ReqData, State, rm_json:segments(SegmentStruct))
     end.
-
-to_json(ReqData, State, Json) ->
-    {mochijson2:encode(Json), ReqData, State}.
 
 %%
 %% Unit Tests
